bdc3ee84d185f238d8074960785378b7ec8b48c7
jordanoboyle <jmcmcode@gmail.com>
2024-05-28 23:38:00 -0600

    Hard coded JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -31,12 +31,12 @@ function PostsNew() {
 }
 
 function PostsIndex(props) {
-  console.log(props.postTitle)
+  console.log(props)
   return (
       <div id="posts-index">
         <h1>All posts</h1>
         <div className="posts">
-            <h2>{props.postTitle}</h2>
+            <h2>Halo 2 Anniversary</h2>
             <p>Date: Month Day, Year</p>
             <img src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/Halo-2-Anniversary-Washed.jpg"/>
             <p>The revamped Anniversary Edition of Halo 2 can be considered a masterpiece that just made the original better</p>
@@ -71,11 +71,34 @@ function Footer() {
 
 function Content() {
   let postTitle = "Super Mario World"
-  // console.log(postTitle)
+  console.log(postTitle)
+  let posts = [
+    {
+      id: 1,
+      title: "Halo 2 Anniversary",
+      body: "The revamped Anniversary Edition of Halo 2 can be considered a masterpiece that just made the original better",
+      image: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/Halo-2-Anniversary-Washed.jpg",
+      date: "Date: January Day, Year"
+    },
+    {
+      id: 2,
+      title: "Super Mario Sunshine, the unsung hero of 3D Mario",
+      body: "While this is always the darkhorse, I would argue it was one of the best entries in the series, with slightly silly controls.",
+      image: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/super-mario-sunshine/6/67/Super-mario-sunshine-1920x1080.jpg?width=1280",
+      date: "Date: May Day, Year"
+    },
+    {
+      id: 3,
+      title: "Turrican, An underrated shoot-em up from the SNES Era",
+      body: "A contender for one of the best 80s based videogame soundtracks really set the tone for this shooter. Contra Clone...maybe, but still great.",
+      image: "https://static.tvtropes.org/pmwiki/pub/images/turrican1-1_7070.png",
+      date: "Date: February Day, Year"
+    },
+  ];
   return (
     <div>
       <PostsNew/>
-      <PostsIndex postTitle={postTitle}/>
+      <PostsIndex posts={posts}/>
     </div>
   )
 }
