4a1750d0cb5d4fc2aa0bff305b8325ecfc083a2d
jordanoboyle <jmcmcode@gmail.com>
2024-05-28 22:47:24 -0600

    Functions for app Header, Footer, PostsIndex, PostsNew

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,80 @@
+function Header() {
+  return (
+    <header>
+      <a href="#">Go To Main Menu</a> | <a href="#posts-index">Load Game Files</a> | <a href="#posts-new">Start New Game</a>
+    </header>
+  )
+}
+
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <p>Hello, I am new to this page!</p>
+      <h1>New post</h1>
+        <form>
+          <div>
+            Post Title: <input type="text"/>
+          </div>
+          <div>
+            Date Entry: <input type="text"/>
+          </div>
+          <div>
+            Record your game data here: <input type="text"/>
+          </div>
+          <div>
+            Place image url here: <input src="url"/>
+          </div>
+          <button type="submit">SAVE YOUR GAME</button>
+        </form>
+    </div>
+  )
+}
+
+function PostsIndex() {
+  return (
+      <div id="posts-index">
+        <h1>All posts</h1>
+        <div className="posts">
+            <h2>Halo 2: Anniversary</h2>
+            <p>Date: Month Day, Year</p>
+            <img src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/Halo-2-Anniversary-Washed.jpg"/>
+            <p>The revamped Anniversary Edition of Halo 2 can be considered a masterpiece that just made the original better</p>
+            <button>More Information</button>    
+        </div>
+        <div className="posts">
+            <h2>Super Mario Sunshine, the unsung hero of 3D Mario</h2>
+            <p>Date: Month Day, Year</p>
+            <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/super-mario-sunshine/6/67/Super-mario-sunshine-1920x1080.jpg?width=1280"/>
+            <p>While this is always the darkhorse, I would argue it was one of the best entries in the series, with slightly silly controls.</p>
+            <button>More Information</button>
+        </div>
+        <div className="posts">
+            <h2>Turrican, An underrated shoot-em up from the SNES Era</h2>
+            <p>Date: Month Day, Year</p>
+            <img src="https://static.tvtropes.org/pmwiki/pub/images/turrican1-1_7070.png"/>
+            <p>A contender for one of the best 80s based videogame soundtracks really set the tone for this shooter. Contra Clone...maybe, but still great.</p>
+            <button>More Information</button>
+        </div>
+      </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 20XX</p>
+      <p>Site by: Jordan O'Boyle</p>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <a href="#">Go To Main Menu</a> | <a href="#posts-index">Load Game Files</a> | <a href="#posts-new">Start New Game</a>
-      </header>
-
-      <div id="posts-new">
-        <p>Hello, I am new to this page!</p>
-        <h1>New post</h1>
-      </div>
-
-      <div id="posts-index">
-        <h1>All posts</h1>
-      </div>
-
-      <footer>
-        <p>Copyright 20XX</p>
-      </footer>
+      <Header />
+      <PostsNew />
+      <PostsIndex />
+      <Footer />
     </div>
   );
 }
