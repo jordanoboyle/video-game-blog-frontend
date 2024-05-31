58e81a581b6631f2f3d4e86c46cce03bd0f3997a
jordanoboyle <jmcmcode@gmail.com>
2024-05-31 13:13:48 -0600

    button for update(non-function), handleUpdate action shows in console

--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,9 +1,33 @@
 export function PostsShow(props) {
+  const handleSubmit = () => {
+    console.log("handling an update submission")
+  }
+  
+  
+  
   return (
     <div>
+      <p>STILL THE CHILLENS HERE</p>
       <p>{props.post.title}</p>
       <p>{props.post.image}</p>
       <p>{props.post.body}</p>
+      <br/>
+      <br/>
+      <br/>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Post Title: <input type="text" name="title"/>
+        </div>
+        <br/>
+        <div>
+          Record your game data here: <input type="text" name="body" />
+        </div>
+        <br/>
+        <div>
+          Place image url here: <input type="text" name="image" />
+        </div>
+        <button type="submit">Resave Game Data</button>
+      </form>
     </div>
   )
 }
