58db49f2c695e2bfb50cab0d5932626d4c159944
jordanoboyle <jmcmcode@gmail.com>
2024-05-30 22:17:41 -0600

    Modal.jsx modal.css and modal button basic function

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,3 +1,4 @@
+import { Modal } from "./Modal";
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
 import axios from "axios";
@@ -5,6 +6,7 @@ import { useState, useEffect } from "react";
 
 export function Content() {
   const [posts, setPosts] = useState([])
+  const [isPostsShowVisible, setIsPostsShowVisible] = useState(false) /*We want to be able to change this dynamically */
 
   const handleIndexPosts = () => {
     axios.get('http://localhost:3000/posts.json').then(response => {
@@ -15,16 +17,25 @@ export function Content() {
   
       })
     }
-
-  useEffect(handleIndexPosts, [])
-
+    useEffect(handleIndexPosts, [])
+    
+    const handleShowPost = () => {
+      console.log("show me the post please")
+    }
+    
+    
   return (
-    <div>
+    <main>
       <PostsNew />
       <br/>
       <br/>
       <button title="Press Here" onClick={handleIndexPosts}> BIG RED BUTTON</button>
+      <button onClick={handleShowPost}>SHOW DIS MODAL</button>
       <PostsIndex posts={posts} />
-    </div>
+      <Modal show={isPostsShowVisible}>  {/*Most of the time a modal will be self closing. */}
+        THHESE BE CHILLENS 
+      </Modal>
+
+    </main>
   );
 }
---
+++ src/Modal.jsx
@@ -0,0 +1,17 @@
+import "./Modal.css";
+
+export function Modal(props) {
+  if (props.show) {
+    return (
+      <div className="modal-background">
+        <section className="modal-main">
+          {props.children}
+          {/*We can put more children here to pass through the modal (used if unchanging MODAL)*/}
+          <button className="close" type="button" onClick={props.onClose}>
+            &#x2715;
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
