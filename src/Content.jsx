6e6c0c20140806d90f668333df8d48143a6eaa2a
jordanoboyle <jmcmcode@gmail.com>
2024-06-10 14:31:14 -0600

    route for login

--- src/Content.jsx
+++ src/Content.jsx
@@ -56,6 +56,7 @@ export function Content() {
     <main>
       <Routes>
         <Route path="/signup" element={ <Signup /> } />
+        <Route path="/login" element={ < Login/> } />
       </Routes>
       <br/>
       <Login />
